99de812d22d32050771dea2b37013863e83f707c
Yuxuan-Lin <yl5445@nyu.edu>
2023-05-05 15:01:54 -0400

    update

--- Tower-Tactics/Assets/Scripts/Core/WaveManager.cs
+++ Tower-Tactics/Assets/Scripts/Core/WaveManager.cs
@@ -70,15 +70,15 @@ public class WaveManager : MonoBehaviour
         Wave wave10 = new Wave { enemyCount = 48, fastCount = 96, heavyCount = 12 };
 
         waves.Add(wave1);
-        // waves.Add(wave2);
-        // waves.Add(wave3);
-        // waves.Add(wave4);
-        // waves.Add(wave5);
-        // waves.Add(wave6);
-        // waves.Add(wave7);
-        // waves.Add(wave8);
-        // waves.Add(wave9);
-        // waves.Add(wave10);
+        waves.Add(wave2);
+        waves.Add(wave3);
+        waves.Add(wave4);
+        waves.Add(wave5);
+        waves.Add(wave6);
+        waves.Add(wave7);
+        waves.Add(wave8);
+        waves.Add(wave9);
+        waves.Add(wave10);
 
         StartCoroutine(SpawnWaveRoutine());
     }
